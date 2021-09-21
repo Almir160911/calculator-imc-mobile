61073cb6157d4a96727a01e312699bfd93543478
Emilio Murta <emilio.murta@msitbrasil.com>
2021-09-21 06:07:59 -0300

    + tabs navigation + Home

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -8,17 +7,7 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import Header from './components/shared/Header.component';
 import Login from './components/login/Login.page';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-
-function Home(props: any) {
-  const { navigation } = props;
-  return <View style={styles.innerContainer}>
-    <Text>Home...</Text>
-    <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-      <Text>Go to Login</Text>
-    </TouchableOpacity>
-  </View>;
-}
+import Home from './components/home/Home.page';
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -41,8 +30,6 @@ export default function App() {
             />
           </Stack.Navigator>
         </NavigationContainer>
-
-
       </View>
     </SafeAreaProvider>
   );
@@ -54,11 +41,4 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column'
   },
-  innerContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: "#fff",
-  }
 });
